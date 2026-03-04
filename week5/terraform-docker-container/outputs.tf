dc2041922f2881da125d7ed81214bde1af335de2
Fakhry <fakhry.ikhsan.firdaus@gmail.com>
2026-03-04 13:04:58 +0700

    fix: error push terraform

--- week5/terraform-docker-container/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "container_id" {
-  description = "ID dari container yang dibuat"
-  value       = docker_container.nginx.id
-}
-
-output "web_url" {
-  description = "URL untuk mengakses Nginx"
-  value       = "http://localhost:${var.external_port}"
-}
